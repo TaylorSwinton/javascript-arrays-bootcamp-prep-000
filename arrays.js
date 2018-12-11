e2692e5babecfb063aa8390bab0ba88be52451b5
Taylor Swinton <taylorswinton1@gmail.com>
2018-12-11 16:46:39 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  var myArray = [element, ...array]
+  return myArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  var myArray = [...array, element]
+  return myArray
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  var myArray = array[index]
+  return myArray
+}
